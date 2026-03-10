f3297d32d49d5258f2d926ced5a4a5e9f649154b
Manav Jain <manav7421@gmail.com>
2026-03-11 00:41:48 +0530

    changes to regular user capabilties

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { login , register , getUsers} = require('../controllers/user.controller');
-const {apiprotectionMiddleware , adminProtectionMiddleware} = require('../middleware/auth');
+const {apiprotectionMiddleware } = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/getusers', apiprotectionMiddleware, adminProtectionMiddleware, getUsers);
+router.get('/getusers', apiprotectionMiddleware, getUsers);
 
 module.exports = router;
